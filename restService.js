190e98863078f140dd064a7229b2ed27d0e12738
Richard Keener <rlkcpo@gmail.com>
2015-04-02 18:54:58 +0200

    Added tabbing to the about page in preperation for FORMS

--- restService.js
+++ restService.js
@@ -1,50 +1,12 @@
-﻿var querystring = require('querystring');
-var https = require('https');
+﻿var Client = require('node-rest-client').Client;
+client = new Client();
 
 
-var host = 'https://ec2-54-145-194-211.compute-1.amazonaws.com:80';
+var REST_SERVER = 'http://ec2-54-145-194-211.compute-1.amazonaws.com';
 
-
-performRequest = function performRequest(endpoint, method, data, success) {
-    var dataString = JSON.stringify(data);
-    var headers = {};
+module.exports = {
     
-    if (method == 'GET') {
-        endpoint += '?' + querystring.stringify(data);
+    getMrxUser: function (email, handler) {
+        client.get(REST_SERVER + '/mrxuser/email/' + email, handler);
     }
-    else {
-        headers = {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-            'Content-Length': dataString.length
-        };
-    }
-    
-    var options = {
-        host: host,
-        port: 443,
-        path: endpoint,
-        method: method,
-        headers: headers
-    };
-    
-    var req = https.request(options, function (res) {
-        res.setEncoding('utf-8');
-        
-        var responseString = '';
-        
-        res.on('data', function (data) {
-            responseString += data;
-        });
-        
-        res.on('end', function () {
-            console.log(responseString);
-            var responseObject = JSON.parse(responseString);
-            success(responseObject);
-        });
-    });
-    
-    req.write(dataString);
-    req.end();
 }
-
---
+++ routes/AutheticatedUser.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET Autheticated User page. */
+router.get('/', function (req, res) {
+    res.render('Welcome ***insert name here***', { title: 'MRX Member Portal' });
+});
+
+module.exports = router;
